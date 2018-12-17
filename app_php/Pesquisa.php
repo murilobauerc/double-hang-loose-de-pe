0eda015e82ca9a67da82877c172be42b2e9e09d5
AlanSoldar <39715854+AlanSoldar@users.noreply.github.com>
2018-12-17 16:32:20 -0200

    melhorando pesquisa - estilo1

--- app_php/Pesquisa.php
+++ app_php/Pesquisa.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<link rel="stylesheet" type="text/css" media="screen" href="css/style.css"/>
+	</head>
 
-	<?php
-		echo "ola mundo!<br/>";
-		$string = $_GET["search"];
-		echo "Você pesquisou por $string";
 
-	?>
+	<body>
+
+		<nav id="navbar">
+	        <ul>
+	            <li>
+	                <h1>
+	                    <a href="index.html"> DHLDP </a>
+	                </h1>
+	                <a href="index2.html"> Próxima Página </a>
+	                <a href="https://github.com/"> Github </a>
+	                
+	            </li>
+	        </ul>
+    	</nav>
+
+		<?php
+			echo "Teste<br/>";
+			$string = $_GET["search"];
+			echo "Você pesquisou por $string";
+
+		?>
+	</body>
 
 </html>
