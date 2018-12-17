f4755b598b850eb72b14c7de294c49f4fb560dbf
AlanSoldar <alan.soledar@hotmail.com>
2018-12-17 21:54:52 -0200

    hidden side bar tentativa 8

--- homeM.php
+++ homeM.php
@@ -7,7 +7,7 @@
         DHLDP
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/styleM.css"/>
     <script src="main.js"></script>
 </head>
 <body>
