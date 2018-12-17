f68a4168eeaadf0d0fe34a873ef48c4abbbcc617
AlanSoledar <alan.soledar@hotmail.com>
2018-12-17 21:09:36 -0200

    correção de erro de index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    function isMobile() {
+        return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+    }
+
+    if(isMobile()){
+        header("Location: http://dhldp-site.herokuapp.com/home.php"); /* Redirect browser */
+        exit();
+    }
+    else{
+        header("Location: http://dhldp-site.herokuapp.com/homeM.php"); /* Redirect browser */
+        exit();
+    }
+
+
+>
